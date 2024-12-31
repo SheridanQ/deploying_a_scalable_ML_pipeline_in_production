95722801aecf254fb80793ab7d5952fe2efece34
SheridanQ <xqi10@hawk.iit.edu>
2024-12-31 14:36:10 -0500

    dvc pull failed

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-if "DYNO" in os.environ and os.path.isdir(".dvc"):
-    os.system("dvc config core.no_scm true")
-    if os.system("dvc pull") != 0:
-        exit("dvc pull failed")
-    os.system("rm -r .dvc .apt/usr/lib/dvc")
+# if "DYNO" in os.environ and os.path.isdir(".dvc"):
+#     os.system("dvc config core.no_scm true")
+#     if os.system("dvc pull") != 0:
+#         exit("dvc pull failed")
+#     os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 
 app = FastAPI()
